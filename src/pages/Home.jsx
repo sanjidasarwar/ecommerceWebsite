4584fc70a42add6b2f4c97b730154187fa31c404
tupsi07 <sanjida657@yahoo.com>
2025-01-14 00:20:50 +0600

    newsletter section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@
 import BestSeller from "../components/BestSeller";
 import Hero from "../components/Hero";
 import LatestCollection from "../components/LatestCollection";
+import Newsletter from "../components/Newsletter";
 import OurPolicy from "../components/OurPolicy";
 
 const Home = () => {
@@ -12,6 +13,7 @@ const Home = () => {
       <LatestCollection />
       <BestSeller />
       <OurPolicy />
+      <Newsletter />
     </div>
   );
 };
